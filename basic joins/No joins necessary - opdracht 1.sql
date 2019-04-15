2a234604d961a0b98183f56cce8266ed194bbfec
Twan Natter <twan180401@gmail.com>
2019-04-15 12:34:46 +0200

    Initial commit

---
+++ basic joins/No joins necessary - opdracht 1.sql
@@ -0,0 +1,12 @@
+SELECT
+tblAuthor.AuthorName,
+tblEpisode.Title,
+tblEpisode.EpisodeType
+FROM
+tblAuthor
+INNER JOIN tblEpisode ON
+tblAuthor.AuthorId = tblEpisode.AuthorId
+WHERE
+tblEpisode.EpisodeType LIKE '%special%'
+ORDER BY
+tblEpisode.Title
